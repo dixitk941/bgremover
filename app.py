1a5593b03e2debbb109ee52e7bb449b172f5a468
Karan Dixit <dixitk941@gmail.com>
2024-10-25 20:48:57 +0530

    Refactor vercel.json to fix destination path for app.py

--- app.py
+++ app.py
@@ -49,4 +49,4 @@ def download_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
